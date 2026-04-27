0768b14a45500270a9cc2f481a06f38cf4a727a4
sijilsasidharan <40237945+sijilsasidharan@users.noreply.github.com>
2026-04-27 09:40:54 +0000

    Add API root endpoint to return available routes

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import JsonResponse
+
+def api_root(request):
+    return JsonResponse({
+        'users': '/users/',
+        'teams': '/teams/',
+        'activities': '/activities/',
+        'leaderboard': '/leaderboard/',
+        'workouts': '/workouts/',
+    })
 
 urlpatterns = [
+    path('', api_root, name='api_root'),
     path('admin/', admin.site.urls),
 ]
